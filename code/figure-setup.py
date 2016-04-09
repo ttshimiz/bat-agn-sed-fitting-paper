6edba0e97cffb2228cee4a701ca877199c98d0df
ttshimiz <tshimizu@astro.umd.edu>
2016-04-09 11:08:30 -0400

    Create setup file for Figures

---
+++ code/figure-setup.py
@@ -0,0 +1,24 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sn
+
+# Set some plotting parameters
+sn.set(context='paper', color_codes=True, style='ticks')
+mpl.rcParams['xtick.labelsize'] = 10
+mpl.rcParams['ytick.labelsize'] = 10
+mpl.rcParams['axes.labelsize'] = 10
+mpl.rcParams['legend.fontsize'] = 8
+mpl.rcParams['text.usetex'] = True
+mpl.rcParams['font.family'] = 'serif'
+mpl.rcParams['font.serif'] = 'Computer Modern Roman'
+mpl.rcParams['mathtext.fontset'] = 'cm'
+mpl.rcParams['ps.fonttype'] = 42                         # Make sure fonts are T1
+mpl.rcParams['pdf.fonttype'] = 42
+
+# Define the optimum widths of figures depending if they will be in a column or 
+# span the whole page
+columnwidth = 240./72.27 # Width of column in LaTeX paper in inches
+textwidth = 504./72.27   # Width of text in LaTeX paper in inches
+
+# Directory to save figures
+figsave_dir = '../figures/'
